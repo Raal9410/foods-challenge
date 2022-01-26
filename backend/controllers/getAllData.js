1cf721a0871a6d8338b4168586f283ee99344d81
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:28:23 -0600

    build tags array that comes from every available tag on the meals

--- backend/controllers/getAllData.js
+++ backend/controllers/getAllData.js
@@ -2,5 +2,7 @@ const { convertCSVToJSON } = require('../utils/getFormattedData')
 
 exports.getAllData = async (req, res) => {
   const data = await convertCSVToJSON('meals.csv')
-  console.log(foods)
+
+  const tags = [...new Set([].concat(...data.map(e => e.tags)))] 
+  console.log(tags) 
 }
