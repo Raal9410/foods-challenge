b98f52b8b3c48e4aad4ef863e622c204fad7c6bc
Raal9410 <rahm9410@gmail.com>
2022-01-26 13:45:07 -0600

    add missing return on routes

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,9 +3,7 @@ import { Context } from '../context/context'
 
 const Dashboard = () => {
   const { meals, tags, mealTypes, fetchData } = useContext(Context)
-  return (
-    <p>Meals App</p>
-  )
+  return <p>Meals App</p>
 }
 
 export default Dashboard
--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
-import { useApi, Context } from '../context/context'
+import { useAPI, Context } from '../context/context'
 import Dashboard from '../components/Dashboard'
 
 const App = () => {
   const routes = useRoutes([{ path: '/', element: <Dashboard /> }])
+  return routes
 }
 
 const AppRouter = () => {
-  const data = useApi()
+  const data = useAPI()
   return (
     <Context.Provider value={data}>
       <BrowserRouter>
