f96bb5aac1988424599c18ac766728200e137a03
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:43:53 -0600

    change how the query params are defined on getAllData route

--- backend/controllers/getAllData.js
+++ backend/controllers/getAllData.js
@@ -1,11 +1,13 @@
 const { convertCSVToJSON } = require('../utils/getFormattedData')
 
 exports.getAllData = async (req, res) => {
-  const {mealType, tags: queryTags} = req.query
-  console.log(mealType, queryTags)
+  const { mealType: selectedMealType, tags: selectedTags } = req.query
+
+  console.log(selectedMealType, selectedTags)
+
   const data = await convertCSVToJSON('meals.csv')
 
-  const tags = [...new Set([].concat(...data.map(e => e.tags)))] 
+  const tags = [...new Set([].concat(...data.map(e => e.tags)))]
   const mealTypes = [...new Set([].concat(...data.map(e => e.mealType)))]
 
   res.send({
