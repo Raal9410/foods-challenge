221b057fe7e769200d9e3f99f781a399bbe2fbd4
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:09:48 -0600

    fix issue related to getting the foods tags array stringified

--- backend/utils/getFormattedData.js
+++ backend/utils/getFormattedData.js
@@ -3,7 +3,10 @@ const csvtojson = require('csvtojson')
 exports.convertCSVToJSON = async file => {
   try {
     const data = await csvtojson().fromFile(file)
-    return data
+    return data.map(e => {
+      e.tags = JSON.parse(e.tags.replaceAll("'", '"'))
+      return e
+    })
   } catch (e) {
     return e
   }
