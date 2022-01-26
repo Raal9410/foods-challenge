e153092b7b4e1d808f43e2324831c2f542e97956
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:46:03 -0600

    retrieve the mealType and the tags for each food to use them to compare with the query params

--- backend/controllers/getAllData.js
+++ backend/controllers/getAllData.js
@@ -3,12 +3,17 @@ const { convertCSVToJSON } = require('../utils/getFormattedData')
 exports.getAllData = async (req, res) => {
   const { mealType: selectedMealType, tags: selectedTags } = req.query
 
-  console.log(selectedMealType, selectedTags)
-
   const data = await convertCSVToJSON('meals.csv')
 
   const tags = [...new Set([].concat(...data.map(e => e.tags)))]
   const mealTypes = [...new Set([].concat(...data.map(e => e.mealType)))]
+
+  const filteredMeals = data.filter(meal => {
+    const { mealType, tags } = meal
+
+    console.log(mealType)
+    console.log(tags)
+  })
 
   res.send({
     tags,
