da65bdc2de5363fb1b993d0ec73142e6de33211c
Raal9410 <rahm9410@gmail.com>
2022-01-26 13:42:01 -0600

    import AppRouter as MealsApp into index.js to render it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import MealsApp from './router/AppRouter'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MealsApp />
   </React.StrictMode>,
   document.getElementById('root'),
 )
