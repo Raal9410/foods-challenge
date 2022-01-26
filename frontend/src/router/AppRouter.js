c250f933088d228f3541fa853724903a670c292d
Raal9410 <rahm9410@gmail.com>
2022-01-26 13:38:37 -0600

    create route for the SPA

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,1 +1,8 @@
 import React from 'react'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
+import { useApi, Context } from '../context/context'
+import Dashboard from '../components/Dashboard'
+
+const App = () => {
+  const routes = useRoutes([{ path: '/', element: <Dashboard /> }])
+}
