be54d91fd80af71147b2222a77d19698581e9348
Raal9410 <rahm9410@gmail.com>
2022-01-26 14:10:26 -0600

    successfully change selectedTags array by using checkboxes

--- frontend/src/components/TagsChekboxes/TagsCheckboxes.js
+++ frontend/src/components/TagsChekboxes/TagsCheckboxes.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
-const TagsCheckBoxes = ({ tags, setSelectedTags }) => {
-
+const TagsCheckBoxes = ({ tags, handleSelectedTags }) => {
   return (
     <div>
       {tags.map(tag => (
         <div key={tag}>
           <label>{tag}</label>
-          <input type="checkbox" value={tag} />
+          <input type="checkbox" value={tag} onChange={e => handleSelectedTags(e)}/>
         </div>
       ))}
     </div>
