3b8ff99818edfe5813822f68bd96e299043fb433
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:19:27 -0600

    move convertCSVToJSON file into foods controller

--- backend/controllers/foods.js
+++ backend/controllers/foods.js
@@ -1,3 +1,6 @@
+const { convertCSVToJSON } = require('../utils/getFormattedData')
+
 exports.getFoods = async (req, res) => {
-  console.log('this is the route')
+  const foods = await convertCSVToJSON('meals.csv')
+  console.log(foods)
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router()
 const { getFoods } = require('../controllers/foods')
-const { convertCSVToJSON } = require('../utils/getFormattedData')
 
-/* GET home page. */
 router.get('/', getFoods)
 
 module.exports = router
