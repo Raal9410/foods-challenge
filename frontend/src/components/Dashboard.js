f7886a9f9bc92c24a2c9eb5d7844b2cb1323c032
Raal9410 <rahm9410@gmail.com>
2022-01-26 13:36:16 -0600

    retrieve data and fetchData from Context

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect } from 'react'
+import { Context } from '../context/context'
 
 const Dashboard = () => {
-
+  const { meals, tags, mealTypes, fetchData } = useContext(Context)
 }
 
 export default Dashboard
