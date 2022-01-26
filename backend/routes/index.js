c4620c210643c84d4911f3839c89eb7c3d7e8fc4
Raal9410 <rahm9410@gmail.com>
2022-01-26 12:01:57 -0600

    add missing async declaration on route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { convertCSVToJSON } = require('../utils/getFormattedData')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/',  async function (req, res, next) {
   const foods = await convertCSVToJSON('meals.csv')
   console.log(foods)
 })
